1fa8b974a0adf2b3bd2906e1bdc59f9ae1153715
trevorhuis <trevorh.9173@gmail.com>
2020-04-15 06:50:43 -0500

    intitial commit

---
+++ src/com/smoothstack/assignments/two/QuestionOne.java
@@ -0,0 +1,11 @@
+package com.smoothstack.assignments.two;
+
+public class QuestionOne {
+    public static void main(String[] args) {
+        
+    }
+
+    public QuestionOne() {
+        
+    }
+}
